859f3f75f9739b9cf1058f986d0ae479ae82b7b3
Daniel Studencki <daniel.studencki@gmail.com>
2019-05-07 18:35:56 +0200

    feat: Added TypeScript support. Closed #48

---
+++ types/highcharts-vue.d.ts
@@ -0,0 +1,42 @@
+import _Vue, { VNode, PropOptions, WatchOptionsWithHandler, WatchHandler } from "vue";
+import Highcharts from "highcharts"
+
+export type ChartUpdateArgs = [boolean, boolean, Highcharts.AnimationOptionsObject]
+
+declare module 'vue/types/options' {
+    interface ComponentOptions<V extends _Vue> {
+        constructorType?: string;
+        options?: Highcharts.Options;
+        callback?: Highcharts.ChartCallbackFunction;
+        updateArgs?: Array<ChartUpdateArgs>;
+        highcharts?: typeof Highcharts;
+        deepCopyOnUpdate?: boolean;
+    }
+}
+
+export interface ChartOptions {
+    tagName?: string;
+    highcharts?: typeof Highcharts;
+}
+
+export interface ChartPropsObject {
+    constructorType: PropOptions;
+    options: PropOptions;
+    updateArgs: PropOptions;
+    callback: () => void;
+}
+
+export interface ChartWatchObject {
+    options: WatchOptionsWithHandler<any>;
+}
+
+export class Chart extends _Vue {
+    props: ChartPropsObject;
+    template: string;
+    watch: ChartWatchObject;
+    beforeDestroy: () => void;
+    mounted: () => void;
+    render: (createElement) => VNode;
+}
+
+export default function install(vue: typeof _Vue, options?: ChartOptions): void;
